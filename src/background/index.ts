7b063ecf91282b2bdf1a06b826daa40a72e640ef
Sinan Bekar <sinanbekar.work@gmail.com>
2022-07-30 18:19:17 +0300

    fix: remove src from html paths hotfix

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,4 +1,5 @@
 import store from '../app/store';
+import { isDev } from '../shared/utils';
 
 store.subscribe(() => {
   console.log('state', store.getState());
@@ -8,7 +9,7 @@ store.subscribe(() => {
 chrome.runtime.onInstalled.addListener(async (details) => {
   if (details.reason === 'install') {
     //show the welcome page
-    const url = chrome.runtime.getURL('src/welcome/welcome.html');
+    const url = chrome.runtime.getURL(isDev ? 'src/welcome/welcome.html' : 'welcome.html'); // TODO: better approach
     await chrome.tabs.create({ url });
   }
 });
