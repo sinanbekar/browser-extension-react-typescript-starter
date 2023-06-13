c4da16aaaab640de0e1fbad542bf99c09909345b
Sinan Bekar <sinanbekar.work@gmail.com>
2023-06-14 01:55:50 +0300

    refactor: src paths removing in the page url

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,6 @@
 import browser from 'webextension-polyfill';
 
 import store from '../app/store';
-import { isDev } from '../shared/utils';
 
 store.subscribe(() => {
   console.log('state', store.getState());
@@ -11,7 +10,7 @@ store.subscribe(() => {
 browser.runtime.onInstalled.addListener(async (details) => {
   if (details.reason === 'install') {
     //show the welcome page
-    const url = browser.runtime.getURL(isDev ? 'src/welcome/welcome.html' : 'welcome.html'); // TODO: better approach
+    const url = browser.runtime.getURL('welcome/welcome.html');
     await browser.tabs.create({ url });
   }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
-import { join } from 'path';
+import { join, resolve } from 'path';
 import { defineConfig } from 'vite';
 
 import manifest from './src/manifest';
-import removeSrcFromHtmlPaths from './utils/plugins/removeSrcFromHtmlPaths';
 
 export default defineConfig({
   // @see https://github.com/crxjs/chrome-extension-tools/issues/696
@@ -15,7 +14,11 @@ export default defineConfig({
       port: 5173,
     },
   },
+  // prevent src/ prefix on extension urls
+  root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'public'),
   build: {
+    outDir: resolve(__dirname, 'dist'),
     rollupOptions: {
       input: {
         welcome: join(__dirname, 'src/welcome/welcome.html'),
@@ -25,5 +28,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), crx({ manifest }), removeSrcFromHtmlPaths()],
+  plugins: [react(), crx({ manifest })],
 });
